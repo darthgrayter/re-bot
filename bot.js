695a5a7d5926b92c35645e45c61adae8450da3cd
darthgrayter <darthgrayter@gmail.com>
2018-05-30 16:52:47 -0400

    updated re'ing

--- bot.js
+++ bot.js
@@ -2,36 +2,62 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomlyConvertToUppercase(str) {
+	if (Math.random() > 0.800) {
+		str.toUpperCase();
+	}
+	else {
+		str.toLowerCase();
+	}
+	return str;
+}
+
+function determineNumOfDupes() {
+	var i = Math.random();
+	var j;
+
+	if (i < 0.850) {
+		j = 1;
+	}
+	else if (i < 0.950) {
+		j = 2;
+	} else {
+		j = 3;
+	}
+	return j;
 }
 
 client.on("ready", () => {
-  console.log("I am ready!");
+	console.log("Leggo bish");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("re")) {
-	var re = "";
-	var lilRe = "";
-	var i = getRndInteger(1, 30);
-	while (i > 0) {
-		lilRe = "";
-		if (Math.random() > 0.500) {
-			lilRe += "R"
-		} else {
-			lilRe += "r"
-		}
+	if (message.content.startsWith("re")) {
+		var re = "";
+		var lilRe = "";
+		var i = getRndInteger(1, 30);
+		while (i > 0) {
+			lilRe = "";
+			var j = determineNumOfDupes();
 
-		if (Math.random() > 0.500) {
-			lilRe += "E"
-		} else {
-			lilRe += "e"
+			while (j > 0) {
+				lilRe += randomlyConvertToUppercase("r");
+				j--;
+			}
+
+			j = determineNumOfDupes();
+			while (j > 0) {
+				lilRe += randomlyConvertToUppercase("e");
+				j--;
+			}
 		}
-		re += lilRe;
-		i--;
+		message.channel.send(re);
 	}
-    message.channel.send(re);
-  }
 });
 
 client.login(process.env.BOT_TOKEN);
+
+
